17ce94efee71e3e5a00968e29d4e59b376559d90
DolceLemus <dulce.lemus.lara@gmail.com>
2018-04-11 10:16:42 -0500

    funciona correctamente la validacion del input

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-//var test = alert("hola!");
 //Input del usuario
 var input = prompt("Ingresa los 16 digitos de tu tarjeta \n ***No debes ingresar espacios(\" \"), letras(abC) o caracteres extranios (&%$)");
 
 //****Validacion del input
-while (input.length === undefined || typeOf(input) === !'Number') {
-  input = prompt("Ingresa los 16 digitos de tu tarjeta \n **Recuerda que no debes ingresar espacios(\" \"), letras(abC) o caracteres extranios (&%$)")
+while (input === null || input.length === 0 || isNaN(input) ) {
+  input = prompt("Ingresa los 16 digitos de tu tarjeta \n **Recuerda que no debes ingresar espacios(\" \"), letras(abC) o caracteres extranios (&%$)");
 }
 
 
